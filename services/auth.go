d482d8c74ecac4f5fb7fc4b345a2133ebe7f8f3e
Mayco Lacerda <maycolacerda@gmail.com>
2025-08-04 21:58:15 -0400

    added: Token generation, validation and extraction

--- services/auth.go
+++ services/auth.go
@@ -40,8 +40,40 @@ func NewAuthRequest(c *gin.Context) {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Email or password is incorrect"})
 			return
 		}
+
+		token, err := GenerateToken(user.UserID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+			return
+		}
+		c.Header("Authorization", token)
 		c.JSON(http.StatusOK, gin.H{"message": "Login successful", "user_id": user.UserID})
 
 	}
 
+}
+
+// CurrentUser godoc
+//
+//	@Summary		Get the currently authenticated user.
+//	@Description	Retrieve the details of the currently authenticated user.
+//	@Tags			Auth
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	models.User
+//	@Failure		401	{object}	map[string]string
+//	@Failure		404	{object}	map[string]string
+//	@Router			/auth/me [get]
+func CurrentUser(c *gin.Context) {
+	userID, err := ExtractTokenID(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	var user models.User
+	if err := database.DB.First(&user, userID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
