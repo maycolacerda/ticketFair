8833bca84f937bc24d54d937807c3bd595629faa
Mayco Lacerda <maycolacerda@gmail.com>
2025-08-07 12:52:11 -0400

    Added: Profile model and profile.validate

--- migration/docker-database-init.sql
+++ migration/docker-database-init.sql
@@ -7,3 +7,17 @@ CREATE TABLE IF NOT EXISTS users (
     `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS profiles (
+    `profile_id` VARCHAR(36) PRIMARY KEY,
+    `user_id` VARCHAR(36) NOT NULL UNIQUE,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NOT NULL,
+    `phone_number` VARCHAR(15) NOT NULL UNIQUE,
+    `address` TEXT,
+    `verified_email` BOOLEAN DEFAULT FALSE,
+    `verified_phone` BOOLEAN DEFAULT FALSE,
+    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
